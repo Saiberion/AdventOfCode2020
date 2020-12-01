c459b83b60ebc4ac4793a0d5e7127c0e1f263161
Matthias Wieloch <saiberion@web.de>
2020-12-01 10:33:47 +0100

    Both parts solved as simple as possible

--- Days/Day01.cs
+++ Days/Day01.cs
@@ -11,10 +11,54 @@ namespace Days
             Load("inputs/day01.txt");
         }
 
+        private string findMatching2Entries(List<int> l)
+        {
+            for (int i = 0; i < l.Count; i++)
+            {
+                for (int j = 0; j < l.Count; j++)
+                {
+                    if (i != j)
+                    {
+                        if ((l[i] + l[j]) == 2020)
+                        {
+                            return (l[i] * l[j]).ToString();
+                        }
+                    }
+                }
+            }
+            return "Error";
+        }
+
+        private string findMatching3Entries(List<int> l)
+        {
+            for (int i = 0; i < l.Count; i++)
+            {
+                for (int j = 0; j < l.Count; j++)
+                {
+                    for (int k = 0; k < l.Count; k++)
+                    {
+                        if ((i != j) && (j != k))
+                        {
+                            if ((l[i] + l[j] + l[k]) == 2020)
+                            {
+                                return (l[i] * l[j] * l[k]).ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            return "Error";
+        }
+
         override public void Solve()
         {
-            Part1Solution = "TBD";
-            Part2Solution = "TBD";
+            List<int> accountingList = new List<int>();
+            foreach (string s in Input)
+            {
+                accountingList.Add(int.Parse(s));
+            }
+            Part1Solution = findMatching2Entries(accountingList);
+            Part2Solution = findMatching3Entries(accountingList);
         }
     }
 }
