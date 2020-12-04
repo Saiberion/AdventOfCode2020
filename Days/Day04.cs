4fc3964431966ae6c1804b9b78337056393988f1
Saiberion <saiberion@web.de>
2020-12-04 17:33:29 +0100

    solved part 1 of day 4

--- Days/Day04.cs
+++ Days/Day04.cs
@@ -11,9 +11,54 @@ namespace Days
             Load("inputs/day04.txt");
         }
 
+        private bool isPassportValid(Dictionary<string, string> p)
+        {
+            return p.ContainsKey("byr") && p.ContainsKey("iyr") && p.ContainsKey("eyr") && p.ContainsKey("hgt") && p.ContainsKey("hcl") && p.ContainsKey("ecl") && p.ContainsKey("pid");
+        }
+
         override public void Solve()
         {
-            Part1Solution = "TBD";
+            List<Dictionary<string, string>> passports = new List<Dictionary<string, string>>();
+            Dictionary<string, string> passport;
+            int validPassports = 0;
+
+            passport = new Dictionary<string, string>();
+            for (int i = 0; i < Input.Count; i++)
+            {
+                if (string.IsNullOrEmpty(Input[i]))
+                {
+                    // blank line --> new passwort
+                    passports.Add(passport);
+                    passport = new Dictionary<string, string>();
+                }
+                else
+                {
+                    string[] splitted = Input[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    foreach (string s in splitted)
+                    {
+                        string[] splitted2 = s.Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (passport.ContainsKey(splitted2[0]))
+                        {
+                            passport[splitted[0]] = splitted[1];
+                        }
+                        else
+                        {
+                            passport.Add(splitted2[0], splitted2[1]);
+                        }
+                    }
+                }
+            }
+            passports.Add(passport);
+
+            foreach(Dictionary<string, string> p in passports)
+            {
+                if (isPassportValid(p))
+                {
+                    validPassports++;
+                }
+            }
+
+            Part1Solution = validPassports.ToString();
             Part2Solution = "TBD";
         }
     }
