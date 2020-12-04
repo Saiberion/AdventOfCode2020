e29b51ca146f98de8c72d8cd653e520c7baad3a1
Saiberion <saiberion@web.de>
2020-12-04 18:03:47 +0100

    part 2 of day 4 solved

--- Days/Day04.cs
+++ Days/Day04.cs
@@ -16,11 +16,95 @@ namespace Days
             return p.ContainsKey("byr") && p.ContainsKey("iyr") && p.ContainsKey("eyr") && p.ContainsKey("hgt") && p.ContainsKey("hcl") && p.ContainsKey("ecl") && p.ContainsKey("pid");
         }
 
+        private bool isPassportDataValid(Dictionary<string, string> p)
+        {
+            int i;
+
+            i = int.Parse(p["byr"]);
+            if ((i < 1920) || (i > 2002))
+            {
+                return false;
+            }
+
+            i = int.Parse(p["iyr"]);
+            if ((i < 2010) || (i > 2020))
+            {
+                return false;
+            }
+
+            i = int.Parse(p["eyr"]);
+            if ((i < 2020) || (i > 2030))
+            {
+                return false;
+            }
+
+            if (p["hgt"].EndsWith("cm"))
+            {
+                i = int.Parse(p["hgt"].TrimEnd(new char[] { 'c', 'm' }));
+                if ((i < 150) || (i > 193))
+                {
+                    return false;
+                }
+            }
+            else if (p["hgt"].EndsWith("in"))
+            {
+                i = int.Parse(p["hgt"].TrimEnd(new char[] { 'i', 'n' }));
+                if ((i < 59) || (i > 76))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+            
+            if (p["hcl"].Length != 7)
+            {
+                return false;
+            }
+            else
+            {
+                if (p["hcl"][0] != '#')
+                {
+                    return false;
+                }
+                else
+                {
+                    string hex = p["hcl"].Remove(0, 1);
+                    if (!int.TryParse(hex, System.Globalization.NumberStyles.HexNumber, null, out i))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (!(p["ecl"].Equals("amb") || p["ecl"].Equals("blu") || p["ecl"].Equals("brn") || p["ecl"].Equals("gry") || p["ecl"].Equals("grn") || p["ecl"].Equals("hzl") || p["ecl"].Equals("oth")))
+            {
+                return false;
+            }
+
+            if (p["pid"].Length != 9)
+            {
+                return false;
+            }
+            else
+            {
+                if (!int.TryParse(p["pid"], System.Globalization.NumberStyles.Integer, null, out i))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         override public void Solve()
         {
             List<Dictionary<string, string>> passports = new List<Dictionary<string, string>>();
             Dictionary<string, string> passport;
             int validPassports = 0;
+            int validPassportsAdvanced = 0;
 
             passport = new Dictionary<string, string>();
             for (int i = 0; i < Input.Count; i++)
@@ -55,11 +139,15 @@ namespace Days
                 if (isPassportValid(p))
                 {
                     validPassports++;
+                    if (isPassportDataValid(p))
+                    {
+                        validPassportsAdvanced++;
+                    }
                 }
             }
 
             Part1Solution = validPassports.ToString();
-            Part2Solution = "TBD";
+            Part2Solution = validPassportsAdvanced.ToString();
         }
     }
 }
