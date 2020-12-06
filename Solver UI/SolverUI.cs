3e001acb31369804ccbcb564c75ce7d33203a190
Matthias Wieloch <saiberion@web.de>
2020-12-06 11:30:33 +0100

    activate day 6 in solver UI

--- Solver UI/SolverUI.cs
+++ Solver UI/SolverUI.cs
@@ -20,7 +20,7 @@ namespace Solver_UI
             new Day03() { Name = "Day 03", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = true },
             new Day04() { Name = "Day 04", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = true },
             new Day05() { Name = "Day 05", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = true },
-            new Day06() { Name = "Day 06", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = false },
+            new Day06() { Name = "Day 06", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = true },
             new Day07() { Name = "Day 07", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = false },
             new Day08() { Name = "Day 08", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = false },
             new Day09() { Name = "Day 09", StopWatch = new System.Diagnostics.Stopwatch(), Enabled = false },
