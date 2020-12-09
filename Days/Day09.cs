162b4218348532355c843312fec72dc498bfcc62
Saiberion <saiberion@web.de>
2020-12-09 12:19:01 +0100

    solved part 1 of day 9

--- Days/Day09.cs
+++ Days/Day09.cs
@@ -11,9 +11,46 @@ namespace Days
             Load("inputs/day09.txt");
         }
 
+        private bool IsValidNumber(long nr, List<long> l)
+        {
+            for (int a = 0; a < (l.Count - 1); a++)
+            {
+                for (int b = a + 1; b < l.Count; b++)
+                {
+                    if ((l[a] + l[b]) == nr)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
         override public void Solve()
         {
-            Part1Solution = "TBD";
+            List<long> encoding = new List<long>();
+            for(int i = 0; i < Input.Count; i++)
+            {
+                if (i < 25)
+                {
+                    encoding.Add(long.Parse(Input[i]));
+                }
+                else
+                {
+                    long nextNr = long.Parse(Input[i]);
+                    if (!IsValidNumber(nextNr, encoding))
+                    {
+                        Part1Solution = nextNr.ToString();
+                        break;
+                    }
+                    else
+                    {
+                        encoding.RemoveAt(0);
+                        encoding.Add(nextNr);
+                    }
+                }
+            }
+
             Part2Solution = "TBD";
         }
     }
