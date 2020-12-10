5e4ff1189a0c8dc108559f7a469db951aa2882a9
Saiberion <saiberion@web.de>
2020-12-10 10:41:15 +0100

    solved part 1 of day 10

--- Days/Day10.cs
+++ Days/Day10.cs
@@ -13,7 +13,30 @@ namespace Days
 
         override public void Solve()
         {
-            Part1Solution = "TBD";
+            List<int> adapters = new List<int>();
+            int diff1 = 0, diff3 = 1;
+
+            foreach(string s in Input)
+            {
+                adapters.Add(int.Parse(s));
+            }
+            adapters.Add(0);
+            adapters.Sort();
+
+            for(int i = 0; i < (adapters.Count - 1); i++)
+            {
+                switch (adapters[i + 1] - adapters[i])
+                {
+                    case 1:
+                        diff1++;
+                        break;
+                    case 3:
+                        diff3++;
+                        break;
+                }
+            }
+
+            Part1Solution = (diff1 * diff3).ToString();
             Part2Solution = "TBD";
         }
     }
