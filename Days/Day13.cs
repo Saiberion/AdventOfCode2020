2ce5c4f3f73cdc8a3b71a87c3c5e692a610aeccb
Matthias Wieloch <saiberion@web.de>
2020-12-13 11:22:37 +0100

    solved part 1 of day 13

--- Days/Day13.cs
+++ Days/Day13.cs
@@ -13,7 +13,29 @@ namespace Days
 
         override public void Solve()
         {
-            Part1Solution = "TBD";
+            int earliestDepartureTime = int.Parse(Input[0]);
+            List<int> busIDs = new List<int>();
+            string[] splitted = Input[1].Split(new char[] { ',', 'x' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach(string s in splitted)
+            {
+                busIDs.Add(int.Parse(s));
+            }
+
+            bool resultFound = false;
+            for (int time = earliestDepartureTime; resultFound == false; time++)
+            {
+                foreach(int b in busIDs)
+                {
+                    if ((time % b) == 0)
+                    {
+                        Part1Solution = ((time - earliestDepartureTime) * b).ToString();
+                        resultFound = true;
+                        break;
+                    }
+                }
+            }
+
+            
             Part2Solution = "TBD";
         }
     }
