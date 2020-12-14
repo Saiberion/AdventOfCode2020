e956824017cad218007a51f46c5fedbd37bd2de1
Matthias Wieloch <saiberion@web.de>
2020-12-14 23:22:09 +0100

    solved part 1 of day 14

--- Days/Day14.cs
+++ Days/Day14.cs
@@ -13,7 +13,39 @@ namespace Days
 
         override public void Solve()
         {
-            Part1Solution = "TBD";
+            long maskAND = 0, maskOR = 0;
+            Dictionary<long, long> memory = new Dictionary<long, long>();
+            foreach(string s in Input)
+            {
+                if (s.StartsWith("mask"))
+                {
+                    string[] splitted = s.Split(new string[] { " = " }, StringSplitOptions.RemoveEmptyEntries);
+                    maskOR = Convert.ToInt64(splitted[1].Replace('X', '0'), 2);
+                    maskAND = Convert.ToInt64(splitted[1].Replace('X', '1'), 2);
+                }
+                else
+                {
+                    string[] splitted = s.Split(new char[] { '[', ']', ' ', '=' }, StringSplitOptions.RemoveEmptyEntries);
+                    long addr = long.Parse(splitted[1]);
+                    long val = (long.Parse(splitted[2]) & maskAND) | maskOR;
+                    if (memory.ContainsKey(addr))
+                    {
+                        memory[addr] = val;
+                    }
+                    else
+                    {
+                        memory.Add(addr, val);
+                    }
+                }
+            }
+
+            long sum = 0;
+            foreach(KeyValuePair<long, long> kvp in memory)
+            {
+                sum += kvp.Value;
+            }
+
+            Part1Solution = sum.ToString();
             Part2Solution = "TBD";
         }
     }
