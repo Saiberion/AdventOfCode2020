46be220c38ec48cdf4b9f880d518c5542d9d5875
Matthias Wieloch <saiberion@web.de>
2020-12-18 14:30:27 +0100

    started to do day 17 but skipped it for now

--- Days/Day17.cs
+++ Days/Day17.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Days
@@ -13,6 +14,31 @@ namespace Days
 
         override public void Solve()
         {
+            Dictionary<Vector3, bool> pocketDimension = new Dictionary<Vector3, bool>();
+
+            for (int y = 0; y < Input.Count; y++)
+            {
+                for (int x = 0; x < Input[y].Length; x++)
+                {
+                    if (Input[y][x] == '#')
+                    {
+                        pocketDimension.Add(new Vector3(x, y, 0), true);
+                    }
+                }
+            }
+
+            float minx = float.MaxValue, maxx = float.MinValue, miny = float.MaxValue, maxy = float.MinValue, minz = float.MaxValue, maxz = float.MinValue;
+            foreach(Vector3 p in pocketDimension.Keys)
+            {
+                minx = Math.Min(minx, p.X) - 1;
+                miny = Math.Min(miny, p.Y) - 1;
+                minz = Math.Min(minz, p.Z) - 1;
+
+                maxx = Math.Max(maxx, p.X) + 1;
+                maxy = Math.Max(maxy, p.Y) + 1;
+                maxz = Math.Max(maxz, p.Z) + 1;
+            }
+            
             Part1Solution = "TBD";
             Part2Solution = "TBD";
         }
